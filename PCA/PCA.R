bd79da0f8bcb0f6391ddfda09fab7e3bd626ca78
Jimi92 <44607500+Jimi92@users.noreply.github.com>
2023-08-03 16:20:19 +0200

    PCA

--- PCA/PCA.R
+++ PCA/PCA.R
@@ -6,45 +6,8 @@ library(gdsfmt)
 library(SNPRelate)
 
 
-
-vcf.fn <- "/home/taliadoros/Desktop/phd_projects/Ptt/New_full/Ptt_filtered.vcf"
-snpgdsVCF2GDS(vcf.fn, "ptt3.gds", method="biallelic.only")
-
-snpgdsSummary("ptt3.gds")
-genofile <- snpgdsOpen("ptt3.gds")
-pca2 <- snpgdsPCA(genofile)
-pc.percent <- pca2$varprop*100
-head(round(pc.percent, 2))
-
-
-tab2 <- data.frame(sample.id = pca2$sample.id,
-                   EV1 = pca2$eigenvect[,1],
-                   # the first eigenvector
-                   EV2 = pca2$eigenvect[,2],
-                   # the second eigenvector
-                   stringsAsFactors = FALSE)
-#with wild
-head(tab2)
-tab2$group <- "ND"
-tab2$group[1:9] <- "IR"
-tab2$group[10:19] <- "ND"
-tab2$group[20:40] <- "FR"
-tab2$group[41:51] <- "AZB"
-tab2$group[52:56] <- "DN"
-tab2$group[57:70] <- "CAW"
-tab2$group[71:80] <- "ND"
-tab2$group[81] <- "IR"
-tab2$group[82:107] <- "IR"
-
-
-tab2$host <- "Domesticated"
-tab2$host[57:70] <- "Wild"
-
-plot(tab2$EV2, tab2$EV1, xlab="eigenvector 1", ylab="eigenvector 2")
-ggplot(data=tab2,aes(x=EV1,y=EV2,color=group, shape=host)) + geom_point(size=2.3) + theme_bw() + labs(title = "                                    P. teres isolates clustering" )  + xlab("PC1 (36.14 %)") + ylab("PC2 (7.64 %)")
-
-#without wild
-vcf.fn <- "/home/taliadoros/Desktop/phd_projects/Ptt/New_full/Domesticated_only_filtered.vcf"
+setwd('/home/taliadoros/Desktop/phd_projects/Cb/Cerco_2023/6_DCA/')
+vcf.fn <- "CB_2023_No_outgroup_miss_indivs_chr1_10_IBS_Biallelic_SNPs.recode.vcf"
 snpgdsVCF2GDS(vcf.fn, "ptt1.gds", method="biallelic.only")
 
 snpgdsSummary("ptt1.gds")
@@ -61,257 +24,111 @@ tab2 <- data.frame(sample.id = pca2$sample.id,
                    # the second eigenvector
                    stringsAsFactors = FALSE)
 
-
-orgn <- read.table("Pyro2.ind2pop")
-tab2$group <- orgn$V1
-#tab2$group <- "ND"
-#tab2$group[1:9] <- "IR"
-#tab2$group[20:40] <- "FR"
-#tab2$group[41:51] <- "AZB"
-#tab2$group[52:56] <- "DN"
-#tab2$group[67] <- "IR"
-#tab2$group[68:93] <- "MR"
-
-ggplot(data=tab2,aes(x=EV1,y=EV2,color=group)) + scale_color_manual(name="Groups", 
-                                                                   values = c("Morocco"="#609CFF","Mixed"="#750000","Iran"="#00BFC4", "North_Dakota"="#F563E3", "Azerbaijan" = "#9BA4A7", "Europe" = "#00BA38"),
-                                                                   labels= c("Morocco"="Morocco","Mixed"="Mixed","Iran"="Iran","North_Dakota"="North_Dakota","Azerbaijan"="Azerbaijan", "Europe"="Europe" )) + geom_point(size=2.3) + theme_bw() + xlab("PC1 (12.93 %)") + ylab("PC2 (8.60 %)")
+org <- read.table('ind_list.txt')
 
 
-#with iranians
-tab2$group <- "ND"
-tab2$group[1:5] <- "IR"
-tab2$group[6:15] <- "ND"
-tab2$group[16:36] <- "FR"
-tab2$group[37:47] <- "AZB"
-tab2$group[48:52] <- "DN"
-tab2$group[53:66] <- "CW"
-tab2$group[67:76] <- "ND"
-tab2$group[77:102] <- "MR"
+tab2['Origin'] <- org$V1
+tab2['Host'] <- org$V2
+pdf('Cb_PCA.pdf')
+ggplot(data=tab2,aes(x=EV1,y=EV2,color=Origin, shape=Host)) + geom_point(size=2.3) + theme_bw() + labs(title = "C. beticola isolates clustering" )  + xlab("PC1 (9.9 %)") + ylab("PC2 (8.37 %)")
+dev.off()
 
-tab2$host[1:52] <- "Domesticated"
-tab2$host[53:66] <- "Wild"
-tab2$host[67:102] <- "Domesticated"
+library(shiny)
+library(plotly)
 
-plot(tab2$EV2, tab2$EV1, xlab="eigenvector 1", ylab="eigenvector 2")
-ggplot(data=tab2,aes(x=EV1,y=EV2,color=group, shape=host)) + geom_point()+ theme_bw() + xlab("PC1 (40.10 %)") + ylab("PC2 (7.89 %)")
+ui <- fluidPage(
+  plotlyOutput("step")
+)
 
-#with iranians no wild
-tab2$group <- "ND"
-tab2$group[1:5] <- "IR"
-tab2$group[6:15] <- "ND"
-tab2$group[16:36] <- "FR"
-tab2$group[37:47] <- "AZB"
-tab2$group[48:52] <- "DN"
-tab2$group[53:62] <- "ND"
-tab2$group[62:88] <- "MR"
+server <- function(input, output) {
+  output$step <- renderPlotly({
+    ggplotly(ggplot(data=tab2,aes(x=EV1,y=EV2,color=Origin, shape=Host)) + geom_point(size=2.3) + theme_bw() + labs(title = "C. beticola isolates clustering" )  + xlab("PC1 (7.43 %)") + ylab("PC2 (6.29 %)")
+)
+  })
+}
 
-ggplot(data=tab2,aes(x=EV1,y=EV2,color=group)) + geom_point()+ theme_bw() + xlab("PC1 (14.4 %)") + ylab("PC2 (9.8 %)")
+shinyApp(ui, server)
 
 
-tab3 <- data.frame(sample.id = pca2$sample.id,
+#uk ONLY
+
+vcf.fn <- "/home/taliadoros/Desktop/phd_projects/Cb/Cerco_2023/4_PCA/CB_UK.recode.vcf"
+snpgdsVCF2GDS(vcf.fn, "ptt1.gds", method="biallelic.only")
+
+snpgdsSummary("ptt1.gds")
+genofile <- snpgdsOpen("ptt1.gds")
+pca2 <- snpgdsPCA(genofile)
+pc.percent <- pca2$varprop*100
+head(round(pc.percent, 2))
+
+
+tab2 <- data.frame(sample.id = pca2$sample.id,
                    EV1 = pca2$eigenvect[,1],
                    # the first eigenvector
-                   EV3 = pca2$eigenvect[,3],
-                   # the second eigenvector
-                   stringsAsFactors = FALSE)
-
-tab3$group <- "ND"
-tab3$group[1:5] <- "IR"
-tab3$group[6:15] <- "ND"
-tab3$group[16:36] <- "FR"
-tab3$group[37:47] <- "AZB"
-tab3$group[48:52] <- "DN"
-tab3$group[53:62] <- "ND"
-tab3$group[62:88] <- "MR"
-
-ggplot(data=tab3,aes(x=EV1,y=EV3,color=group)) + geom_point()+ theme_bw() + xlab("PC1 (14.4 %)") + ylab("PC3 (6.0 %)")
-
-
-tab4 <- data.frame(sample.id = pca2$sample.id,
                    EV2 = pca2$eigenvect[,2],
-                   # the first eigenvector
-                   EV3 = pca2$eigenvect[,3],
                    # the second eigenvector
                    stringsAsFactors = FALSE)
 
-tab4$group <- "ND"
-tab4$group[1:5] <- "IR"
-tab4$group[6:15] <- "ND"
-tab4$group[16:36] <- "FR"
-tab4$group[37:47] <- "AZB"
-tab4$group[48:52] <- "DN"
-tab4$group[53:62] <- "ND"
-tab4$group[62:88] <- "MR"
+tab2['Host'] <- "Seabeet"
+tab2[22:37,4] <- "Sugarbeet"
 
-ggplot(data=tab4,aes(x=EV2,y=EV3,color=group)) + geom_point()+ theme_bw() + xlab("PC2 (9.8 %)") + ylab("PC3 (6.0 %)")
+library(shiny)
+library(plotly)
 
-tab5 <- data.frame(sample.id = pca2$sample.id,
+ui <- fluidPage(
+  plotlyOutput("step")
+)
+
+server <- function(input, output) {
+  output$step <- renderPlotly({
+    ggplotly(ggplot(data=tab2,aes(x=EV1,y=EV2,color=Host, shape=Host)) + geom_point(size=2.3) + theme_bw() + labs(title = "C. beticola isolates clustering" )  + xlab("PC1 (7.43 %)") + ylab("PC2 (6.29 %)")
+    )
+  })
+}
+
+shinyApp(ui, server)
+
+ggplot(data=tab2,aes(x=EV1,y=EV2,color=Host, shape=Host)) + geom_point(size=2.3) + theme_bw() + labs(title = "C. beticola isolates clustering from UK" )  + xlab("PC1 (20.53 %)") + ylab("PC2 (11.34 %)")
+
+
+
+
+vcf.fn <- "/home/taliadoros/Desktop/phd_projects/Cb/Cerco_2023/4_PCA/CB_Wild.recode.vcf"
+snpgdsVCF2GDS(vcf.fn, "ptt6.gds", method="biallelic.only")
+
+snpgdsSummary("ptt6.gds")
+genofile <- snpgdsOpen("ptt6.gds")
+pca2 <- snpgdsPCA(genofile)
+pc.percent <- pca2$varprop*100
+head(round(pc.percent, 2))
+
+
+tab2 <- data.frame(sample.id = pca2$sample.id,
                    EV1 = pca2$eigenvect[,1],
                    # the first eigenvector
-                   EV4 = pca2$eigenvect[,4],
+                   EV2 = pca2$eigenvect[,2],
                    # the second eigenvector
                    stringsAsFactors = FALSE)
 
-tab5$group <- "ND"
-tab5$group[1:5] <- "IR"
-tab5$group[6:15] <- "ND"
-tab5$group[16:36] <- "FR"
-tab5$group[37:47] <- "AZB"
-tab5$group[48:52] <- "DN"
-tab5$group[53:62] <- "ND"
-tab5$group[62:88] <- "MR"
-
-ggplot(data=tab5,aes(x=EV1,y=EV4,color=group)) + geom_point()+ theme_bw() + xlab("PC1 (14.4 %)") + ylab("PC4 (4.2 %)")
+org <- read.table('out/pop_info.txt')
 
 
-#admixture analysis
-tbl = read.table("/home/taliadoros/Desktop/PhD_projects/Ptt/VCF/Admixture_thin/plink.2.Q")
-#tbl$site <- ifelse(tbl$V2 < 0.0001, "USA", "MOR")
+tab2['Host'] <- "Croatia"
+tab2[16:36,4] <- "UK"
 
-tbl$site <- "ND"
-tbl$site[11:31] <- "FR"
-tbl$site[32:42] <- "AZB"
-tbl$site[43:47] <- "DN"
-tbl$site[48:61] <- "CW"
-tbl$site[62:71] <- "ND"
-tbl$site[72:97] <- "MR"
-ord = tbl[order(tbl$V1,tbl$V2),]
 
-barNaming <- function(vec) {
-  retVec <- vec
-  for(k in 2:length(vec)) {
-    if(vec[k-1] == vec[k])
-      retVec[k] <- ""
-  }
-  return(retVec)
+
+ggplot(data=tab2,aes(x=EV1,y=EV2,color=Host, shape=Host)) + geom_point(size=2.3) + theme_bw() + labs(title = "C. beticola isolates from sea beet clustering" )  + xlab("PC1 (13.81 %)") + ylab("PC2 (10.71 %)")
+
+ui <- fluidPage(
+  plotlyOutput("step")
+)
+
+server <- function(input, output) {
+  output$step <- renderPlotly({
+    ggplotly(ggplot(data=tab2,aes(x=EV1,y=EV2,color=Host, shape=Host)) + geom_point(size=2.3) + theme_bw() + labs(title = "C. beticola isolates from sea beet clustering" )  + xlab("PC1 (13.81 %)") + ylab("PC2 (10.71 %)")
+    )
+  })
 }
-barplot(t(as.matrix(ord)), col=rainbow(7), xlab="Sampling site", main = "Admixture", ylab="Ancestry", border=NA, names.arg=barNaming(ordered(ord$site)), las=3)
-
-#admixture analysis
-tbl = read.table("/home/taliadoros/Desktop/PhD_projects/Ptt/VCF/Admixture_thin/plink.3.Q")
-#tbl$site <- ifelse(tbl$V2 < 0.0001, "USA", "MOR")
-
-tbl$site <- "ND"
-tbl$site[11:31] <- "FR"
-tbl$site[32:42] <- "AZB"
-tbl$site[43:47] <- "DN"
-tbl$site[48:61] <- "CW"
-tbl$site[62:71] <- "ND"
-tbl$site[72:97] <- "MR"
-#ord = tbl[order(tbl$V1,tbl$V2,tbl$V3),]
-ord = tbl[order(tbl$site),]
-barNaming <- function(vec) {
-  retVec <- vec
-  for(k in 2:length(vec)) {
-    if(vec[k-1] == vec[k])
-      retVec[k] <- ""
-  }
-  return(retVec)
-}
-barplot(t(as.matrix(ord)), col=rainbow(7), xlab="Sampling site", main = "Admixture", ylab="Ancestry", border=NA, names.arg=barNaming(ordered(ord$site)), las=3)
-
-tbl = read.table("/home/taliadoros/Desktop/PhD_projects/Ptt/VCF/Admixture_thin/plink.4.Q")
-#tbl$site <- ifelse(tbl$V2 < 0.0001, "USA", "MOR")
-
-tbl$site <- "ND"
-tbl$site[11:31] <- "FR"
-tbl$site[32:42] <- "AZB"
-tbl$site[43:47] <- "DN"
-tbl$site[48:61] <- "CW"
-tbl$site[62:71] <- "ND"
-tbl$site[72:97] <- "MR"
-#ord = tbl[order(tbl$V1,tbl$V2,tbl$V3,tbl$V4),]
-ord = tbl[order(tbl$site),]
-barNaming <- function(vec) {
-  retVec <- vec
-  for(k in 2:length(vec)) {
-    if(vec[k-1] == vec[k])
-      retVec[k] <- ""
-  }
-  return(retVec)
-}
-barplot(t(as.matrix(ord)), col=rainbow(6), xlab="Sampling site", main = "Admixture", ylab="Ancestry", border=NA, names.arg=barNaming(ordered(ord$site)), las=3)
-
-tbl = read.table("/home/taliadoros/Desktop/PhD_projects/Ptt/VCF/Admixture_thin/plink.5.Q")
-#tbl$site <- ifelse(tbl$V2 < 0.0001, "USA", "MOR")
-
-tbl$site <- "ND"
-tbl$site[11:31] <- "FR"
-tbl$site[32:42] <- "AZB"
-tbl$site[43:47] <- "DN"
-tbl$site[48:61] <- "CW"
-tbl$site[62:71] <- "ND"
-tbl$site[72:97] <- "MR"
-#ord = tbl[order(tbl$V1,tbl$V2,tbl$V3,tbl$V4,tbl$V5),]
-ord = tbl[order(tbl$site),]
-barNaming <- function(vec) {
-  retVec <- vec
-  for(k in 2:length(vec)) {
-    if(vec[k-1] == vec[k])
-      retVec[k] <- ""
-  }
-  return(retVec)
-}
-barplot(t(as.matrix(ord)), col=rainbow(6), xlab="Sampling site", main = "Admixture", ylab="Ancestry", border=NA, names.arg=barNaming(ordered(ord$site)), las=3)
-
-tbl = read.table("/home/taliadoros/Desktop/PhD_projects/Ptt/VCF/Admixture_thin/plink.6.Q")
-#tbl$site <- ifelse(tbl$V2 < 0.0001, "USA", "MOR")
-
-tbl$site <- "ND"
-tbl$site[11:31] <- "FR"
-tbl$site[32:42] <- "AZB"
-tbl$site[43:47] <- "DN"
-tbl$site[48:61] <- "CW"
-tbl$site[62:71] <- "ND"
-tbl$site[72:97] <- "MR"
-#ord = tbl[order(tbl$V1,tbl$V2,tbl$V3,tbl$V4,tbl$V5,tbl$V6),]
-ord = tbl[order(tbl$site),]
-barNaming <- function(vec) {
-  retVec <- vec
-  for(k in 2:length(vec)) {
-    if(vec[k-1] == vec[k])
-      retVec[k] <- ""
-  }
-  return(retVec)
-}
-barplot(t(as.matrix(ord)), col=rainbow(7), xlab="Sampling site", main = "Admixture", ylab="Ancestry", border=NA, names.arg=barNaming(ordered(ord$site)), las=3)
-
-tbl = read.table("/home/taliadoros/Desktop/PhD_projects/Ptt/VCF/Admixture_thin/plink.7.Q")
-
-tbl$site <- "ND"
-tbl$site[11:31] <- "FR"
-tbl$site[32:42] <- "AZB"
-tbl$site[43:47] <- "DN"
-tbl$site[48:61] <- "CW"
-tbl$site[62:71] <- "ND"
-tbl$site[72:97] <- "MR"
-#ord = tbl[order(tbl$V1,tbl$V2,tbl$V3,tbl$V4,tbl$V5,tbl$V6),]
-ord = tbl[order(tbl$site),]
-barplot(t(as.matrix(ord)), col=rainbow(7), xlab="Sampling site", main = "Admixture", ylab="Ancestry", border=NA, names.arg=barNaming(ordered(ord$site)), las=3)
-
-tbl = read.table("/home/taliadoros/Desktop/PhD_projects/Ptt/VCF/Admixture_thin/plink.8.Q")
-
-tbl$site <- "ND"
-tbl$site[11:31] <- "FR"
-tbl$site[32:42] <- "AZB"
-tbl$site[43:47] <- "DN"
-tbl$site[48:61] <- "CW"
-tbl$site[62:71] <- "ND"
-tbl$site[72:97] <- "MR"
-#ord = tbl[order(tbl$V1,tbl$V2,tbl$V3,tbl$V4,tbl$V5,tbl$V6),]
-ord = tbl[order(tbl$site),]
-barplot(t(as.matrix(ord)), col=rainbow(8), xlab="Sampling site", main = "Admixture", ylab="Ancestry", border=NA, names.arg=barNaming(ordered(ord$site)), las=3)
-
-tbl = read.table("/home/taliadoros/Desktop/PhD_projects/Ptt/VCF/Admixture_thin/plink.9.Q")
-
-tbl$site <- "ND"
-tbl$site[11:31] <- "FR"
-tbl$site[32:42] <- "AZB"
-tbl$site[43:47] <- "DN"
-tbl$site[48:61] <- "CW"
-tbl$site[62:71] <- "ND"
-tbl$site[72:97] <- "MR"
-#ord = tbl[order(tbl$V1,tbl$V2,tbl$V3,tbl$V4,tbl$V5,tbl$V6),]
-ord = tbl[order(tbl$site),]
-barplot(t(as.matrix(ord)), col=rainbow(9), xlab="Sampling site", main = "Admixture", ylab="Ancestry", border=NA, names.arg=barNaming(ordered(ord$site)), las=3)
-
+shinyApp(ui, server)
+                                                                                                                                                                                
